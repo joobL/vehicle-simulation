3ce426296c2a2d92d180119dcd0f14e218704d26
joobs <3486473062@qq.com>
2023-07-05 10:33:47 +0800

    车联数据模拟+netty

---
+++ netty-server/target/classes/static/vehicle/js/muyu.js
@@ -0,0 +1,109 @@
+(function ($) {
+    $.extend({
+// 弹出层封装处理
+        modal: {
+            // 显示图标
+            icon: function (type) {
+                var icon = "";
+                if (type == modal_status.WARNING) {
+                    icon = 0;
+                } else if (type == modal_status.SUCCESS) {
+                    icon = 1;
+                } else if (type == modal_status.FAIL) {
+                    icon = 2;
+                } else {
+                    icon = 3;
+                }
+                return icon;
+            },
+            // 消息提示
+            msg: function (content, type) {
+                if (type != undefined) {
+                    layer.msg(content, {icon: $.modal.icon(type), time: 2000, shift: 5});
+                } else {
+                    layer.msg(content);
+                }
+            },
+            // 错误消息
+            msgError: function (content) {
+                $.modal.msg(content, modal_status.FAIL);
+            },
+            // 成功消息
+            msgSuccess: function (content) {
+                $.modal.msg(content, modal_status.SUCCESS);
+            },
+            // 警告消息
+            msgWarning: function (content) {
+                $.modal.msg(content, modal_status.WARNING);
+            },
+            // 弹出提示
+            alert: function (content, type) {
+                layer.alert(content, {
+                    icon: $.modal.icon(type),
+                    title: "系统提示",
+                    btn: ['确认'],
+                    btnclass: ['btn btn-primary'],
+                });
+            },
+            // 消息提示并刷新父窗体
+            msgReload: function (msg, type) {
+                layer.msg(msg, {
+                        icon: $.modal.icon(type),
+                        time: 500,
+                        shade: [0.1, '#8F8F8F']
+                    },
+                    function () {
+                        $.modal.reload();
+                    });
+            },
+            // 错误提示
+            alertError: function (content) {
+                $.modal.alert(content, modal_status.FAIL);
+            },
+            // 成功提示
+            alertSuccess: function (content) {
+                $.modal.alert(content, modal_status.SUCCESS);
+            },
+            // 警告提示
+            alertWarning: function (content) {
+                $.modal.alert(content, modal_status.WARNING);
+            },
+        },
+        muyuSend : {
+            send:function (url, type, data, fun){
+                $.ajax({
+                    url : url,
+                    type : type,
+                    dataType:"json",
+                    contentType:"application/json",
+                    data : data == undefined ? null : JSON.stringify(data),
+                    success : fun
+                })
+            },
+            post:function (url, data, fun) {
+                $.muyuSend.send(url,"POST",data,fun);
+            },
+            get:function (url, data, fun) {
+                $.muyuSend.send(url,"GET",data,fun);
+            }
+        }
+    })
+})(jQuery);
+/** 弹窗状态码 */
+modal_status = {
+    SUCCESS: "success",
+    FAIL: "error",
+    WARNING: "warning"
+};
+
+function getFormToMap(formId){
+    var vehiceleForm = $("#"+formId).serialize();
+    var arrs =vehiceleForm.split("&");
+    var paramJson = {};
+    for (var i in arrs){
+        var arr = arrs[i];
+        var arrSplit = arr.split("=");
+        paramJson[arrSplit[0]] = arrSplit.length > 1 ? arrSplit[1] : "";
+    }
+    return paramJson;
+}
